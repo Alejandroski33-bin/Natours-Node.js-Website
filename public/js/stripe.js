7063e14236fd9cd2cccc7f1de8705aaec4118470
Alejandro Castro <alecafer99@gmail.com>
2026-06-17 14:05:17 -0600

    fixed bug

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,11 +1,11 @@
 /* eslint-disable */
 import axios from 'axios';
 import { showAlert } from './alerts';
+const stripe = Stripe(
+  'pk_test_51Tik3WCLUyoMBqOldK3aIyGkBPv1gjoXdrOkpFbAKmMF8BqtakxycgF8crFusiOA0g4QhEX2BFuIiyx6AJZQ3BZP009JXPEULn',
+);
 
 export const bookTour = async (tourId) => {
-  const stripe = Stripe(
-    'pk_test_51Tik3WCLUyoMBqOldK3aIyGkBPv1gjoXdrOkpFbAKmMF8BqtakxycgF8crFusiOA0g4QhEX2BFuIiyx6AJZQ3BZP009JXPEULn',
-  );
   try {
     // 1) Get checkout session from API
     const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
