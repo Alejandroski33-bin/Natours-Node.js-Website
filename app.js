fa7124ddd2e653b53da6aa78d1c87536a4f577d1
Alejandro Castro <alecafer99@gmail.com>
2026-06-17 00:51:05 -0600

    modified p.json

--- app.js
+++ app.js
@@ -37,13 +37,13 @@ app.use(
   }),
 );
 
-mongoose
-  .connect('your_mongodb_uri', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('Could not connect to MongoDB', err));
+// mongoose
+//   .connect('your_mongodb_uri', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log('Connected to MongoDB'))
+//   .catch((err) => console.error('Could not connect to MongoDB', err));
 
 // Further HELMET configuration for Security Policy (CSP)
 const scriptSrcUrls = [
